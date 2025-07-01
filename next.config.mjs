2246859c22022c7a260d77fb0b0005845bd9e0a7
erfan sharafi <erfansharafi60@gmail.com>
2025-07-01 10:02:36 +0330

    refactor: comment out API rewrites in next.config.mjs for future use

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // تمام درخواست‌های /api/* در فرانت‌اند
-        destination: "http://192.168.66.3:3000/api/:path*", // به سرور بک‌اند هدایت می‌شوند
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*", // تمام درخواست‌های /api/* در فرانت‌اند
+  //       destination: "http://192.168.66.3:3000/api/:path*", // به سرور بک‌اند هدایت می‌شوند
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
